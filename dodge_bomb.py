6ab7ddd3c5ea8fbc711538afd56f305f9dc09829
daiki <c0c2508016@edu.teu.ac.jp>
2026-04-21 14:23:37 +0900

    練習2：爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {pg.K_UP : (0,-5),pg.K_DOWN : (0,5),pg.K_LEFT : (-5,0), pg.K_RIGHT: (5,0),}
@@ -15,8 +15,16 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img, (255,0,0), (10,10), 10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+    vx,vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -29,7 +37,7 @@ def main():
             if key_lst[key]:
                  sum_mv[0] += mv[0]
                  sum_mv[1] += mv[1]
-                 
+
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -40,6 +48,8 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img, bb_rct) #爆弾を表示させる
         pg.display.update()
         tmr += 1
         clock.tick(50)
